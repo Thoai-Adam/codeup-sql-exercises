700125f76060f3ac3b3478394e330dc983206dd4
Thoai Adam Nguyen <nguyenphithoaihung@gmail.com>
2023-06-26 13:53:37 -0500

    updated after reviewing answers

--- db_tables_exercises.sql
+++ db_tables_exercises.sql
@@ -10,11 +10,16 @@ SHOW TABLES;
 -- Which tables do you think contain a numeric type column? * every tables except departments
 -- Which table do you think contain a string type column? * every table except salaries 
 -- which tables do you think contain a date type column? * every table except dept_emp table
--- what is the relationship between the employees and the departments table?
+-- what is the relationship between the employees and the departments table? * There is no direct relationship between these two tables
 -- Show the SQL that created the dept_manager table.
-CREATE TABLE dept_manager(
-	emp_no int,
-    dept_no char(4),
-    from_date date,
-    to_date date,
-    );
+SHOW CREATE TABLE dept_manager;
+CREATE TABLE `dept_manager` (
+  `emp_no` int NOT NULL,
+  `dept_no` char(4) NOT NULL,
+  `from_date` date NOT NULL,
+  `to_date` date NOT NULL,
+  PRIMARY KEY (`emp_no`,`dept_no`),
+  KEY `dept_no` (`dept_no`),
+  CONSTRAINT `dept_manager_ibfk_1` FOREIGN KEY (`emp_no`) REFERENCES `employees` (`emp_no`) ON DELETE CASCADE ON UPDATE RESTRICT,
+  CONSTRAINT `dept_manager_ibfk_2` FOREIGN KEY (`dept_no`) REFERENCES `departments` (`dept_no`) ON DELETE CASCADE ON UPDATE RESTRICT
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
