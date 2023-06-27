2d12bff19ca253da573a4e710efe7d410fb85bd1
Thoai Adam Nguyen <nguyenphithoaihung@gmail.com>
2023-06-27 14:02:06 -0500

    updating exercise for order_by_excercise

--- basic_statement_exercises.sql
+++ basic_statement_exercises.sql
@@ -8,12 +8,14 @@ SELECT * FROM albums;
 SELECT * FROM albums WHERE sales >= 20;
 
 
-
 -- WHAT IS THE PRIMARY KEY FOR THE ALBUM TABLE?  * 
-		-- id
+		-- "id"
+-- WHAT DOES THE COLUMN NAMED 'NAME' REPRESENT?
+		-- The name of the album
 -- WHAT DO YOU THINK THE SALES COLUMN REPRESENTS? * 
 		-- the amount of tickets sold for by the millions
 -- FIND THE NAME FO ALL ALBUMS BY PINK FLOYD. * 
+		-- SELECT * FROM albums WHERE artist = "Pink Floyd";
 		-- The Dark Side of the Moon and The Wall
 -- WHAT IS THE YEAR SGT. PEPPER'S LONELY HEARTS CLUB BAND WAS RELEASED?  * 
 		-- SELECT * FROM albums WHERE name = 'SGT. PEPPER\'s LONELY HEARTS CLUB BAND';
@@ -21,8 +23,13 @@ SELECT * FROM albums WHERE sales >= 20;
 -- WHAT IS THE GENRE FOR THE ALBUMS NEVERMIND? * 
 		-- SELECT * FROM albums WHERE name = 'NEVERMIND';
 		-- Grunge, Alternative rock
--- WHICH ALBUMS WERE RELEASED IN THE 1960s? *SELECT * 
-		-- FROM albums WHERE release_date >= 1960 AND release_date <= 1969; 
+-- WHICH ALBUMS WERE RELEASED IN THE 1990s? *SELECT * 
+		-- FROM albums WHERE release_date >= 1990 AND release_date <= 1999; 
 -- WHICH ALBUMS HAD LESS THAN 20 MILLION CERTIFIED SALES. RENAMED THIS COLUMN AS LOW_SELLING_ALBUM * 
 		-- SELECT DISTINCT sales AS LOW_SELLING_ALBUM FROM albums;
-        -- SELECT * FROM albums WHERE sales >= 20;
+        -- SELECT * FROM albums WHERE sales <= 20;
+        
+        -- SELECT name AS low_selling_albums FROM albums WHERE sales < 20;
+-- Write a statement with today's vocab:
+		-- Select * FROM Somerville WHERE Last_Name ('Nguyen','Zalazar');
+        -- will bring up Adam and Marcelino
---
+++ order_by_exercises.sql
@@ -0,0 +1,121 @@
+SHOW DATABASES;
+USE employees;
+SELECT database ();
+SHOW TABLES;
+
+
+-- Find all current or previous employees with first names 'Irena', 'Vidya', or 'Maya' using IN. What is the employee number of the top three results? *10200, 10397, 10610
+SELECT emp_no, first_name
+FROM employees
+WHERE first_name IN ('Irena','Vidya', 'Maya');
+-- Find all current or previous employees with first names 'Irena', 'Vidya', or 'Maya', as in Q2, but use OR instead of IN. What is the employee number of the top three results? Does it match the previous question? * 10200, 10397, 10610
+SELECT emp_no, first_name
+FROM employees
+WHERE first_name = 'Irena'
+	OR first_name = 'Maya'
+    OR first_name = 'Vidya';
+-- Find all current or previous employees with first names 'Irena', 'Vidya', or 'Maya', using OR, and who is male. What is the employee number of the top three results. * 17494, 18945, 19058
+SELECT emp_no, first_name, gender
+FROM employees
+WHERE first_name IN ('Irena' 'Vidya')
+	OR first_name IN ('Maya')
+    AND gender IN ('M');
+    -- 10200, 10397, 10821
+-- Find all unique last names that start with 'E'.
+SELECT DISTINCT last_name
+FROM employees
+WHERE last_name LIKE 'E%';
+-- Find all unique last names that start or end with 'E'.
+SELECT DISTINCT last_name
+FROM employees
+WHERE last_name LIKE 'E%' OR '%E';
+-- Find all unique last names that end with E, but does not start with E?
+SELECT DISTINCT last_name
+FROM employees
+WHERE last_name LIKE '%E' AND last_name NOT LIKE 'E%';
+-- Find all unique last names that start and end with 'E'.
+SELECT DISTINCT last_name
+FROM employees 
+WHERE last_name LIKE 'E%E';
+-- Find all current or previous employees hired in the 90s. Enter a comment with top three employee numbers. * 10008, 10011, 10012
+SELECT hire_date, emp_no, first_name
+FROM employees
+WHERE hire_date LIKE '199%-%%-%%';  -- could be written as '199%'
+-- Find all current or previous employees born on Christmas. Enter a comment with top three employee numbers. 10078, 10115, 10261
+SELECT hire_date, emp_no, first_name
+FROM employees
+WHERE birth_date LIKE '%%%%-12-25'; -- could be written as '%12-25'
+-- Find all current or previous employees hired in the 90s and born on Christmas. Enter a comment with top three employee numbers.
+SELECT hire_date, emp_no, first_name
+FROM employees
+WHERE hire_date LIKE '%%%%-12-25'                 -- could be written as '100%' AND birth_date LIKE '%12-25';
+	AND birth_date LIKE '199%-%%-%%';
+-- Find all unique last names that have a 'q' in their last name.
+SELECT DISTINCT last_name
+FROM employees
+WHERE last_name LIKE '%q%';
+-- Find all unique last names that have a 'q' in their last name but not 'qu'.
+SELECT DISTINCT last_name
+FROM employees
+WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';    
+
+
+
+
+-- Find all employees with first names 'Irena', 'Vidya', or 'Maya', 
+-- and order your results returned by first name. In your comments, answer: 
+-- What was the first and last name in the first row of the results? * Irena Reutenauer
+-- What was the first and last name of the last person in the table? * Maya Melter
+SELECT first_name, last_name, emp_no
+FROM employees
+WHERE first_name = 'Irena'
+	OR first_name = 'Maya'
+    OR first_name = 'Vidya'
+ORDER BY first_name;
+-- Find all employees with first names 'Irena', 'Vidya', or 'Maya', 
+-- and order your results returned by first name and then last name. 
+-- In your comments, answer: What was the first and last name in the first row of the results?  *Irena Acton
+-- What was the first and last name of the last person in the table? Vidya, Zwizig
+SELECT first_name, last_name, emp_no
+FROM employees
+WHERE first_name = 'Irena'
+	OR first_name = 'Maya'
+    OR first_name = 'Vidya'
+ORDER BY first_name ASC, last_name ASC;
+-- Find all employees with first names 'Irena', 'Vidya', or 'Maya', 
+-- and order your results returned by last name and then first name. 
+-- In your comments, answer: What was the first and last name in the first row of the results? *Irena Acton
+-- What was the first and last name of the last person in the table? Maya, Zyda
+SELECT last_name, first_name, emp_no
+FROM employees
+WHERE first_name = 'Irena'
+	OR first_name = 'Maya'
+    OR first_name = 'Vidya'
+ORDER BY last_name ASC, first_name ASC;
+-- Write a query to to find all employees whose last name starts and ends with 'E'. 
+-- Sort the results by their employee number. Enter a comment with the number of employees returned,  * 899 rows returned
+-- the first employee number and their first and last name, * 10021 Ramzi Erde
+-- and the last employee number with their first and last name. * 499648 Erde Tadahiro
+SELECT last_name, first_name, emp_no
+FROM employees
+WHERE last_name LIKE 'E%E'
+ORDER BY emp_no;
+
+-- Write a query to to find all employees whose last name starts and ends with 'E'. 
+-- Sort the results by their hire date, so that the newest employees are listed first. 
+-- Enter a comment with the number of employees returned, the name of the newest employee,  *1000 rows returned
+-- and the name of the oldest employee. Cristinel Erdi 1985-02-02 no 485389
+SELECT last_name, first_name, hire_date, emp_no
+FROM employees
+WHERE last_name LIKE 'E%'
+ORDER BY hire_date;
+-- Find all employees hired in the 90s and born on Christmas. 
+-- Sort the results so that the oldest employee who was hired last is the first result. 
+-- Enter a comment with the number of employees returned, the name of the oldest employee who was hired last, *346 returned.
+		-- oldest employee who was hired last is Lucian Menhoudj
+-- and the name of the youngest employee who was hired first.
+		-- Lena Lenart
+SELECT last_name, first_name, hire_date, birth_date, emp_no
+FROM employees
+WHERE hire_date LIKE '199%-12-25'
+ORDER BY birth_date DESC, hire_date ASC;
