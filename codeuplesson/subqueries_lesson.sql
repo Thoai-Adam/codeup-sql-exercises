9efd93b91402cecb4e9a62bbbfc7d281690c3452
Thoai Adam Nguyen <nguyenphithoaihung@gmail.com>
2023-06-30 11:01:54 -0500

    added subqueries exercise. finished join exercises

---
+++ codeuplesson/subqueries_lesson.sql
@@ -0,0 +1,19 @@
+#Scalar subqueries
+#Use albums_db!
+USE albums_db;
+
+#What albums had above-average sales?
+
+
+#Column subqueries
+#Use employees!
+USE employees;
+
+#Can we combine information from employees and managers without joins?
+
+
+#Table subqueries
+#Use employees!
+USE employees;
+
+#We can join to the results of a subquery!
